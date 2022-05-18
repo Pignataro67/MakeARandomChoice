5bfc919b6eff08e09d7f1c48f283002fb964b51d
Pignataro67 <Pignataro67@gmail.com>
2022-05-18 05:03:59 -0700

    adding JS

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
+  if(e.key === 'Enter') {
+    setTimeout(() => {
+        e.target.value = ''
+      }, 10)
 
+     randomSelect()
   }
 })
